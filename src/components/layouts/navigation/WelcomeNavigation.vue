d368b6d0fa7280be523d0e7ee78b00b5c9a7ab1f
hotsaucejake <get@hotsaucejake.com>
2023-01-30 11:49:03 -0500

    nav color

--- src/components/layouts/navigation/WelcomeNavigation.vue
+++ src/components/layouts/navigation/WelcomeNavigation.vue
@@ -4,7 +4,7 @@ import ThemeToggle from "@/components/ThemeToggle.vue";
 </script>
 <template>
   <nav
-    class="border-b-4 border-b-blue-400 shadow-md shadow-slate-400 dark:shadow-slate-600 bg-slate-100 dark:bg-slate-800 text-slate-800 dark:text-slate-100 sticky top-0 z-40"
+    class="border-b-4 opacity-90 border-b-blue-400 shadow-md shadow-slate-400 dark:shadow-slate-600 bg-gradient-to-r from-slate-100 to-blue-100 dark:from-slate-800 dark:to-blue-900 text-slate-800 dark:text-slate-100 sticky top-0 z-40"
   >
     <div class="mx-auto px-4">
       <div class="flex justify-between">
