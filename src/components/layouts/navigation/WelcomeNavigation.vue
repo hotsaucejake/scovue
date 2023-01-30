3554b5c1ad6a0deb7d44257a493ac8d5603b5264
hotsaucejake <get@hotsaucejake.com>
2023-01-29 22:54:05 -0500

    theme toggle

--- src/components/layouts/navigation/WelcomeNavigation.vue
+++ src/components/layouts/navigation/WelcomeNavigation.vue
@@ -1,27 +1,30 @@
+<script setup lang="ts">
+import ThemeToggle from "@/components/ThemeToggle.vue";
+import { useDark, useToggle } from "@vueuse/core";
+
+const isDark = useDark();
+const toggleDark = useToggle(isDark);
+</script>
 <template>
-  <nav class="bg-white shadow-lg">
+  <nav class="bg-transparent border-b-4 border-b-slate-100 shadow-lg">
     <div class="max-w-6xl mx-auto px-4">
       <div class="flex justify-between">
         <div class="flex space-x-7">
           <!--Website Logo-->
 
           <a href="#" class="flex items-center py-4 px-2">
-            <img
-              src="images/devwares-logo.png"
-              alt="Logo"
-              class="h-8 w-8 mr-2"
-            />
             <span class="font-semibold text-gray-500 text-lg">
               Navigation
             </span>
           </a>
         </div>
         <div class="hidden md:flex items-center space-x-1">
+          <ThemeToggle />
           <a
             href=""
             class="py-4 px-2 text-gray-500 border-b-4 border-purple-500 font-semibold"
           >
-            Home
+            Home1
           </a>
           <a
             href=""
