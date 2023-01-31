c6e4753d553166a40b44708e914e4e7ee65811a3
hotsaucejake <get@hotsaucejake.com>
2023-01-31 10:31:26 -0500

    logo updates

--- src/views/WelcomeView.vue
+++ src/views/WelcomeView.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
+import JotSauceLogo from "@/components/JotSauceLogo.vue";
 import WelcomeLayout from "@/components/layouts/WelcomeLayout.vue";
 </script>
 
 <template>
   <WelcomeLayout>
+    
     <p class="m-20">slot</p>
+    <JotSauceLogo :size="'300'" />
     <p class="m-20 fill-fuchsia-400">slot</p>
     <p class="m-20">slot</p>
     <p class="m-20">slot</p>
