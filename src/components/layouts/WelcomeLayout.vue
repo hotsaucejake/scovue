ffa290f8cc7c79b107d06f8fcb0e17d95429cd2b
hotsaucejake <get@hotsaucejake.com>
2023-02-01 12:47:29 -0500

    fonts and welcome poc

--- src/components/layouts/WelcomeLayout.vue
+++ src/components/layouts/WelcomeLayout.vue
@@ -4,9 +4,19 @@ import WelcomeNavigation from "./navigation/WelcomeNavigation.vue";
 <template>
   <section>
     <div
-      class="h-screen min-h-min w-screen bg-light-desk dark:bg-dark-desk bg-cover bg-slate-100 dark:bg-slate-800 text-slate-800 dark:text-slate-100"
+      class="h-screen min-h-min w-screen bg-light-desk dark:bg-dark-desk bg-cover bg-slate-100 dark:bg-slate-800 text-slate-800 dark:text-slate-100 font-dosis-regular"
     >
       <WelcomeNavigation :is-sticky="false" :is-transparent="true" />
+      <div class="flex justify-center h-full">
+        <h2 class="font-dosis-bold text-7xl md:text-8xl lg:text-9xl my-auto">
+          <span class="font-arvo-bold text-transparent bg-clip-text bg-gradient-to-br from-fuchsia-400 to-blue-400">
+            <nobr>Finally,</nobr></span>
+            <br />
+            <nobr>a digital
+          <span class="font-satisfy-regular font-black text-blue-400">journal</span></nobr>
+          <br />
+          <nobr>for <span class="text-fuchsia-400">everything.</span></nobr></h2>
+      </div>
     </div>
     <slot></slot>
   </section>
--- src/views/WelcomeView.vue
+++ src/views/WelcomeView.vue
@@ -13,9 +13,34 @@ import WelcomeLayout from "@/components/layouts/WelcomeLayout.vue";
     </section>
     <WelcomeNavigation :is-sticky="true" :is-transparent="false" />
     <section>
-      blah blah blah
-      <p class="m-36">blah blah blah</p>
-      <p class="m-36">blah blah blah</p>
+
+      <p class="m-36 text-9xl font-dosis-regular">
+        <span class="font-arvo-bold">Finally,</span><br> 
+        a digital <span class="font-satisfy-regular">journal</span><br> 
+        for everything.</p>
+      
+      <p class="m-36 font-dosis-bold text-2xl">jotsauce JotSauce JOTSAUCE</p>
+      <p class="m-36 font-dosis-extra-bold text-2xl">
+        jotsauce JotSauce JOTSAUCE
+      </p>
+      <p class="m-36 font-dosis-extra-light text-2xl">
+        jotsauce JotSauce JOTSAUCE
+      </p>
+      <p class="m-36 font-dosis-light text-2xl">jotsauce JotSauce JOTSAUCE</p>
+      <p class="m-36 font-dosis-medium text-2xl">jotsauce JotSauce JOTSAUCE</p>
+      <p class="m-36 font-dosis-regular text-2xl">jotsauce JotSauce JOTSAUCE</p>
+      <p class="m-36 font-dosis-semi-bold text-2xl">
+        jotsauce JotSauce JOTSAUCE
+      </p>
+      <p class="m-36 font-arvo-regular text-2xl">jotsauce JotSauce JOTSAUCE</p>
+      <p class="m-36 font-arvo-bold text-2xl">jotsauce JotSauce JOTSAUCE</p>
+      <p class="m-36 font-arvo-italic text-2xl">jotsauce JotSauce JOTSAUCE</p>
+      <p class="m-36 font-arvo-bold-italic text-2xl">
+        jotsauce JotSauce JOTSAUCE
+      </p>
+      <p class="m-36 font-satisfy-regular text-2xl">
+        jotsauce JotSauce JOTSAUCE
+      </p>
       <p class="m-36">blah blah blah</p>
       <p class="m-36">blah blah blah</p>
       <p class="m-36">blah blah blah</p>
