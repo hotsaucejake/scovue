eab204c2b1b9c0f44e87c498461ec3a8abed6ee2
hotsaucejake <get@hotsaucejake.com>
2023-03-07 22:17:46 -0500

    quasi working login

--- src/services/jotsauce/auth.service.ts
+++ src/services/jotsauce/auth.service.ts
@@ -1,10 +1,10 @@
 import { postAsync } from '@/composables/useJotSauceApi';
-import type { AxiosResponse } from 'axios';
 import type { AuthenticationResponseInterface } from './interfaces/auth/authentication-response.interface';
 import type { UserCredentialsInterface } from './interfaces/auth/user-credentials.interface';
+import type { ServiceResponse } from './interfaces/service-response.interface';
 
 const baseUrl = 'api/auth/';
 
-export async function login(userCredentials: UserCredentialsInterface): Promise<AxiosResponse<AuthenticationResponseInterface>> {
-  return await postAsync<AxiosResponse>(baseUrl + 'login', userCredentials);
+export async function login(userCredentials: UserCredentialsInterface): Promise<ServiceResponse<AuthenticationResponseInterface>> {
+  return await postAsync<AuthenticationResponseInterface>(baseUrl + 'login', userCredentials);
 }
--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -5,19 +5,73 @@ import { login } from '@/services/jotsauce/auth.service';
 import { object, string } from 'zod';
 import { useUserSession } from '@/stores/userSession';
 import { notify } from 'notiwind';
-import { onMounted, ref } from 'vue';
+import { onMounted, readonly, ref, toRaw } from 'vue';
+import type { UserCredentialsInterface } from '@/services/jotsauce/interfaces/auth/user-credentials.interface';
+import type { AuthenticationResponseInterface } from '@/services/jotsauce/interfaces/auth/authentication-response.interface';
+import { useRouter, useRoute } from 'vue-router';
 
+const isLoading = ref(false);
+const router = useRouter();
+const route = useRoute();
 const userSession = useUserSession();
 userSession.clearAuthentication(); // clear authentication if they ever hit this page
 
-const authentication = ref({
-  email_or_username: '',
-  password: '',
+const authentication = ref<UserCredentialsInterface>({
+  email_or_username: 'b',
+  password: 'b',
 });
 
-// const authenticationSchema = object({
-//   email_or_username: string().min(3).max(255).required(),
-// });
+const authenticationSchema = object({
+  email_or_username: string().min(3).max(255),
+  password: string().min(8),
+}).required();
+
+async function handleLogin() {
+  if (!isLoading.value) {
+    isLoading.value = true;
+
+    try {
+      const validation = authenticationSchema.safeParse(toRaw(authentication.value));
+
+      if (!validation.success) {
+        console.log(validation);
+        isLoading.value = false;
+        return;
+      } else {
+        const response = login(authentication.value);
+
+        if ((await response).type === 'data' && (await response).data?.plainTextToken) {
+          userSession.setAuthentication((await response).data as AuthenticationResponseInterface);
+
+          if (route.redirectedFrom !== undefined) {
+            router.push(route.redirectedFrom);
+          } else {
+            router.push({ path: '/index' });
+          }
+        } else if ((await response).type === 'error' && 'errors' in (await response).data!) {
+          // Object.values((await response).data!.errors).forEach((val) => {
+          //   val.forEach((error) => {
+          //     notify(
+          //       {
+          //         group: 'top-right',
+          //         title: 'Error',
+          //         text: error,
+          //       },
+          //       6000
+          //     );
+          //   });
+          // });
+        } else {
+          console.log(await response);
+        }
+      }
+    } catch (e) {
+      console.log(e);
+    }
+
+    isLoading.value = false;
+  }
+}
 
 // import { object, string, number } from 'zod';
 
@@ -50,27 +104,7 @@ const authentication = ref({
 // }
 
 onMounted(async () => {
-  // await login({ email_or_username: 'hotsaucejakes', password: 'password' });
-  // await login({ email_or_username: 'hotsaucejake', password: 'password' });
-  // Error notifcation
-  notify(
-    {
-      group: 'top-right',
-      title: 'Error',
-      text: 'Your email is already used!',
-    },
-    999999
-  );
-
-  notify(
-    {
-      group: 'top-right',
-      title: 'Error',
-      text: 'Your email is already used!',
-    },
-    4000
-  );
-
+  console.log(authenticationSchema.safeParse(toRaw(authentication.value)));
   // Generic notification
   // notify(
   //   {
@@ -85,7 +119,7 @@ onMounted(async () => {
 
 <template>
   <section class="w-full h-screen bg-center bg-cover bg-wood-section font-dosis-regular font-semibold text-slate-800 dark:text-slate-100 text-lg">
-    <!-- <LoadingScreen /> -->
+    <LoadingScreen v-if="isLoading"></LoadingScreen>
     <div class="w-full h-full bg-gradient-to-tr from-slate-100 dark:from-slate-800 flex justify-center items-center flex-col">
       <div class="flex justify-center">
         <div class="relative py-3 w-80 sm:max-w-xl md:max-w-2xl sm:mx-auto">
@@ -97,7 +131,7 @@ onMounted(async () => {
               login
             </div>
             <div class="px-6 mt-6">
-              <div class="relative z-0 mb-6 w-full group">
+              <div class="relative z-0 mb-6 w-full">
                 <input
                   v-model="authentication.email_or_username"
                   type="email"
@@ -111,10 +145,11 @@ onMounted(async () => {
                   class="absolute px-1 text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-2 z-1 origin-[0] left-3 peer-focus:text-blue-400 peer-focus:dark:text-blue-500 bg-slate-100 dark:bg-slate-800 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                   >email / username</label
                 >
+                <div class="text-red-500 text-sm text-left w-full mt-1">Required</div>
               </div>
             </div>
             <div class="px-6 mt-3">
-              <div class="relative z-0 mb-6 w-full group">
+              <div class="relative z-0 mb-6 w-full">
                 <input
                   v-model="authentication.password"
                   type="password"
@@ -128,6 +163,14 @@ onMounted(async () => {
                   class="absolute px-1 text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-2 z-1 origin-[0] left-3 peer-focus:text-blue-400 peer-focus:dark:text-blue-500 bg-slate-100 dark:bg-slate-800 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                   >password</label
                 >
+              </div>
+              <div class="px-6 mt-3 mb-6">
+                <button
+                  @click="handleLogin"
+                  class="inline-flex mx-3 px-6 py-3 text-center justify-center text-lg text-slate-100 transition duration-300 rounded-full hover:from-blue-600 hover:to-fuchsia-600 ease bg-gradient-to-br from-blue-400 to-fuchsia-400 w-36"
+                >
+                  Login
+                </button>
               </div>
             </div>
             <div class="py-3 px-6 border-t border-slate-400 dark:border-slate-500">
