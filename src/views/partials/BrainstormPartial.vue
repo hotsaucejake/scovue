ce0fce01138dce5971f188952fd45c91ad0b6bd7
hotsaucejake <get@hotsaucejake.com>
2023-03-21 14:33:40 -0400

    welcome partial views

---
+++ src/views/partials/BrainstormPartial.vue
@@ -0,0 +1,107 @@
+<template>
+  <section class="text-lg max-w-6xl mx-auto">
+    <p class="mt-8">JotSauce by Developepper, a hotsaucejake company.</p>
+    <p class="mt-8">
+      I'm an avid (analog) journaler for organization, productivity, and reflection. You can learn a lot about yourself and personal growth by revisiting years'
+      worth of your own logging - you get out of it what you put into it. I log everything from spontaneous to-do lists, various things from my job, daily
+      journaling/reflection, notes from articles / books, anything worth writing down - whether meaningful or trivial, etc... If you're familiar with the bullet
+      journal, it's similar to that with my own flare. To the naked eye it looks like chaos, but it's very well organized. I use symbols to differentiate
+      between different entries and an index in the front of the journal to keep track of the pages those symbols appear - it takes less than 5 minutes at the
+      end of everyday to add the page numbers for each type of entry in the index. It's a type of reflection at the end of the day. Now I can go to previous
+      years' journals and view specific entries by looking in the index for all the pages those entries might appear (my weight, the cost of gas, daily
+      reflection entries, what I did for work that day, etc...).
+    </p>
+    <p class="mt-8">
+      The only con or downfall to this method, and the reason I started a create a digital version of it, is you can't easily "search" for entries by keyword or
+      visual progress if the types of entries are numeric, dates, or some form of stat. For instance, I want to be able to graph my weight, or the price of gas
+      at the time of purchase, or any other type of entry that can indicate status or progress. Plus, what if I want to add photos I took during the day with my
+      phone to an entry? Obviously, you can't do that with a physical journal.
+    </p>
+    <p class="mt-8">
+      So I set out to create an app that can do all of this. I believe this app will benefit anybody who uses it, regardless of background or profession with
+      the ability to customize it to their own needs.
+    </p>
+    <p class="mt-8">
+      It may sound somewhat complex, but it's pretty simple. You create the type of entry you want to make in the beginning and record your information with
+      that entry. Examples would include a daily journal entry (title, date, free form text field, and possibly image uploads), a daily weight entry (date,
+      numeric weight input), etc... It goes a couple steps further with the ability to tag individual entries and organize them into a collection no matter the
+      type of entries. If you're an author writing a book, you might have entry types for ideas, citations, website bookmarks, actual chapters, etc... all fall
+      under one type of collection you can easily view.
+    </p>
+    <p class="mt-8">
+      Welcome to JotSauce, the digital journaling app that makes it easy to log and track all aspects of your life. Whether you're an avid journaler,
+      productivity enthusiast, or someone looking to improve their personal growth, our app is designed to help you organize your thoughts and keep track of
+      your progress.
+    </p>
+    <p class="mt-8">
+      Here are some of the features that set JotSauce apart:<br />
+
+      Customizable Entry Types: Create entry types for anything you want to track, such as daily journal entries, weight logs, expenses, and more.<br />
+      Tagging and Collections: Organize your entries into collections and tag them for easy reference.<br />
+      Graphing and Data Visualization: Visualize your progress over time with customizable graphs and charts.<br />
+      Image Uploads: Add photos or images to your entries to give them more context and detail.<br />
+      Powerful Search: Easily find specific entries by searching for keywords or tags.
+    </p>
+    <p class="mt-8">
+      With JotSauce, you can easily track your progress and reflect on your growth over time. Whether you're looking to improve your health, track your
+      finances, or just keep a daily journal, our app has everything you need to succeed.
+    </p>
+    <p class="mt-8">
+      Here are some examples of how you can use JotSauce:<br />
+
+      Track your weight and see your progress over time with customizable graphs and charts.<br />
+      Log your daily journal entries, complete with text fields and image uploads to document your day.<br />
+      Keep track of your expenses and see where your money is going with detailed analytics.<br />
+      Record your ideas and inspirations with customizable entry types and collections.<br />
+      Monitor your productivity and see how much you're getting done each day with detailed time logs.
+    </p>
+    <p class="mt-8">JotSauce is the ultimate tool for anyone looking to improve their organization, productivity, and personal growth.</p>
+    <p class="mt-8">
+      Some examples might include: Tracking their daily weight. Logging miles commuted for business expenses. Logging the cost of gas at the time of purchase
+      and how many gallons purchased when they fill up their car. A daily journal for them to reflect on their day that might also include photos from that day.
+      Keeping track of their investments. How much they spend and on what in order to track spending habits. What their workout routine was for the day. Keeping
+      a collection of website bookmarks so they can easily find articles or websites again. Writing down notes, outlines, or actual chapters for a novel they're
+      writing. Creating to-do lists. Keeping track of accounts and passwords as a sort of password manager. Writing down earliest known memories. Keeping a
+      daily journal about their child growing up. Keeping a log of everything about vacations which could include expenses, destinations, and memories.
+    </p>
+    <p class="mt-8">
+      Calories consumed or burned during the day<br />
+      Sleep quality and duration<br />
+      Water intake<br />
+      Medications taken and dosages<br />
+      Mood and emotions<br />
+      Books read or movies watched<br />
+      Gifts given and received<br />
+      Ideas for projects or businesses<br />
+      Things they're grateful for each day<br />
+      Daily affirmations or mantras<br />
+      Recipes they've tried or created<br />
+      Skills they've learned or want to learn<br />
+      Places they've traveled to or want to travel to<br />
+      Daily spending, categorized by type (food, entertainment, etc.)<br />
+      Memories or stories from their childhood or family history.
+    </p>
+
+    <p class="m-36 text-9xl font-dosis-regular">
+      <span class="font-arvo-bold">Finally,</span><br />
+      a digital <span class="font-satisfy-regular">journal</span><br />
+      for everything.
+    </p>
+
+    <p class="m-36 font-dosis-bold text-4xl">jotsauce JotSauce JOTSAUCE font-dosis-bold</p>
+    <p class="m-36 font-dosis-extra-bold text-4xl">jotsauce JotSauce JOTSAUCE font-dosis-extra-bold</p>
+    <p class="m-36 font-dosis-extra-light text-4xl">jotsauce JotSauce JOTSAUCE font-dosis-extra-light</p>
+    <p class="m-36 font-dosis-light text-4xl">jotsauce JotSauce JOTSAUCE font-dosis-light</p>
+    <p class="m-36 font-dosis-medium text-4xl">jotsauce JotSauce JOTSAUCE font-dosis-medium</p>
+    <p class="m-36 font-dosis-regular text-4xl">jotsauce JotSauce JOTSAUCE font-dosis-regular</p>
+    <p class="m-36 font-dosis-semi-bold text-4xl">jotsauce JotSauce JOTSAUCE font-dosis-semi-bold</p>
+    <p class="m-36 font-arvo-regular text-4xl">jotsauce JotSauce JOTSAUCE font-arvo-regular</p>
+    <p class="m-36 font-arvo-bold text-4xl">jotsauce JotSauce JOTSAUCE font-arvo-bold</p>
+    <p class="m-36 font-arvo-italic text-4xl">jotsauce JotSauce JOTSAUCE font-arvo-italic</p>
+    <p class="m-36 font-arvo-bold-italic text-4xl">jotsauce JotSauce JOTSAUCE font-arvo-bold-italic</p>
+    <p class="m-36 font-satisfy-regular text-4xl">jotsauce JotSauce JOTSAUCE font-satisfy-regular</p>
+    <!-- <div
+        class="h-full bg-slate-100 opacity-50 dark:bg-slate-800 dark:opacity-50"
+      ></div> -->
+  </section>
+</template>
---
+++ src/views/partials/FeaturesPartial.vue
@@ -0,0 +1,80 @@
+<template>
+  <section class="py-24 sm:py-24">
+    <div class="mx-auto max-w-7xl px-6 lg:px-8">
+      <div class="mx-auto max-w-2xl sm:text-center">
+        <h2 class="text-xl md:text-2xl xl:text-3xl font-semibold leading-7 text-slate-100">There When You Need to Jot Something Down</h2>
+        <p class="mt-2 text-3xl sm:text-4xl md:text-5xl font-bold text-gray-900">Keep a digital log of everything</p>
+        <p class="mt-6 text-xl md:text-2xl leading-8 text-slate-800">
+          JotSauce is a digital journaling app that makes it easy to log and track all aspects of your life. Whether you're an avid journaler, productivity
+          enthusiast, or someone looking to improve their personal growth, our app is designed to help you organize your thoughts and keep track of your
+          progress.
+        </p>
+      </div>
+      <div class="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
+        <dl class="grid max-w-xl grid-cols-1 gap-y-10 gap-x-8 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
+          <div class="relative pl-16">
+            <dt class="text-lg sm:text-2xl font-bold leading-7 text-slate-800">
+              <div class="absolute top-0 left-0 flex h-10 w-10 items-center justify-center rounded-lg bg-blue-700">
+                <svg class="h-6 w-6 text-slate-100" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M2 6c0-1.505.78-3.08 2-4 0 .845.69 2 2 2 1.657 0 3 1.343 3 3 0 .386-.08.752-.212 1.09.74.594 1.476 1.19 2.19 1.81L8.9 11.98c-.62-.716-1.214-1.454-1.807-2.192C6.753 9.92 6.387 10 6 10c-2.21 0-4-1.79-4-4zm12.152 6.848l1.34-1.34c.607.304 1.283.492 2.008.492 2.485 0 4.5-2.015 4.5-4.5 0-.725-.188-1.4-.493-2.007L18 9l-2-2 3.507-3.507C18.9 3.188 18.225 3 17.5 3 15.015 3 13 5.015 13 7.5c0 .725.188 1.4.493 2.007L3 20l2 2 6.848-6.848c1.885 1.928 3.874 3.753 5.977 5.45l1.425 1.148 1.5-1.5-1.15-1.425c-1.695-2.103-3.52-4.092-5.448-5.977z"
+                  />
+                </svg>
+              </div>
+              Customizable Entry Types
+            </dt>
+            <dd class="mt-2 text-xl leading-7 text-slate-700">
+              Create entry types for anything you want to track, such as daily journal entries, weight logs, expenses, and more.
+            </dd>
+          </div>
+
+          <div class="relative pl-16">
+            <dt class="text-lg sm:text-2xl font-extrabold leading-7 text-slate-800">
+              <div class="absolute top-0 left-0 flex h-10 w-10 items-center justify-center rounded-lg bg-blue-700">
+                <svg class="h-6 w-6 text-slate-100" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M20 2.007h-7.087c-.53 0-1.04.21-1.414.586L2.592 11.5c-.78.78-.78 2.046 0 2.827l7.086 7.086c.78.78 2.046.78 2.827 0l8.906-8.906c.376-.374.587-.883.587-1.413V4.007c0-1.105-.895-2-2-2zM17.007 9c-1.105 0-2-.895-2-2s.895-2 2-2 2 .895 2 2-.895 2-2 2z"
+                  />
+                </svg>
+              </div>
+              Tagging and Compositions
+            </dt>
+            <dd class="mt-2 text-xl leading-7 text-slate-700">Organize your entries into compositions and tag them for easy reference.</dd>
+          </div>
+
+          <div class="relative pl-16">
+            <dt class="text-lg sm:text-2xl font-bold leading-7 text-slate-800">
+              <div class="absolute top-0 left-0 flex h-10 w-10 items-center justify-center rounded-lg bg-blue-700">
+                <svg class="h-6 w-6 text-slate-100" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M21 21H3v-2h18v2zM8 10H4v7h4v-7zm6-4h-4v11h4V6zm6-3h-4v14h4V3z" />
+                </svg>
+              </div>
+              Graphing and Data Visualization
+            </dt>
+            <dd class="mt-2 text-xl leading-7 text-slate-700">Visualize your progress over time with customizable graphs and charts.</dd>
+          </div>
+
+          <div class="relative pl-16">
+            <dt class="text-lg sm:text-2xl font-bold leading-7 text-slate-800">
+              <div class="absolute top-0 left-0 flex h-10 w-10 items-center justify-center rounded-lg bg-blue-700">
+                <svg class="h-6 w-6 text-slate-100" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M21 19l-5.154-5.154C16.574 12.742 17 11.42 17 10c0-3.866-3.134-7-7-7s-7 3.134-7 7 3.134 7 7 7c1.42 0 2.742-.426 3.846-1.154L19 21l2-2zM5 10c0-2.757 2.243-5 5-5s5 2.243 5 5-2.243 5-5 5-5-2.243-5-5z"
+                  />
+                </svg>
+              </div>
+              Powerful Search
+            </dt>
+            <dd class="mt-2 text-xl leading-7 text-slate-700">Easily find specific entries by searching for keywords or tags.</dd>
+          </div>
+        </dl>
+      </div>
+    </div>
+  </section>
+</template>
