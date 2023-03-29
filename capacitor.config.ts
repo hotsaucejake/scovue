13a9287a7c1fda041f012312836dc93ae5f1a56c
hotsaucejake <get@hotsaucejake.com>
2023-03-29 19:50:57 -0400

    working ionic capacitor project

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.jotsauce',
+  appName: 'jotsauce',
+  webDir: '../jotsauce/public/scovue',
+  bundledWebRuntime: false,
+};
+
+export default config;
