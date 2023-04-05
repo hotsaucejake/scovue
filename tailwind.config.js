cf73592cc98a05cd5a0fd858e80706db87ea795b
hotsaucejake <get@hotsaucejake.com>
2023-04-04 23:54:16 -0400

    add flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 // eslint-disable-next-line no-undef
 module.exports = {
   darkMode: 'class',
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}', './node_modules/flowbite/**/*.js'],
   theme: {
     extend: {
       backgroundImage: {
@@ -43,5 +43,6 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  // eslint-disable-next-line no-undef
+  plugins: [require('flowbite/plugin')],
 };
